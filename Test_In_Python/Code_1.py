6e9f01f1938d9b59ca3e44516bf70090f0b9ff50
davi898 <davialbuquerque998@gmail.com>
2020-06-29 16:19:37 -0400

    Pandas
    I have finally imported the pandas package

--- Test_In_Python/Code_1.py
+++ Test_In_Python/Code_1.py
@@ -1,8 +1,6 @@
 import pandas as pd
-group_1 = {'Name': ['Jane', 'Julia', 'Carl', 'Kevin', 'Scott', 'Lina', 'Cindy'],
-'Gender':['F', 'F', 'M', 'M', 'M', 'M', 'F', 'F', 'F', 'F', 'M', 'F', 'M', 'F'],
-'Scores' : [902, 608, 654, 708, 993, 658, 879, 579, 871, 657, 702, 998, 890]}
-df = pd.Dataframe(group_1)
-print('Done')
-print('END')
-print('Hi')
+group_1 = {'Name': ['Jane', 'Julia', 'Carl', 'Kevin', 'Scott', 'Lina', 'Cindy', 'Carol'],
+'Gender':['F', 'F', 'M', 'M', 'M', 'F', 'F', 'F'],
+'Scores' : [902, 608, 654, 708, 993, 658, 879, 579]}
+df = pd.DataFrame(group_1)
+print(df)
